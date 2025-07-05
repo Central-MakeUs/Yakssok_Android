0c7a46aa129a86dfd8e46ff01e751f00466eadbc
Inwoo <lys91892@gmail.com>
2025-07-05 22:24:04 +0900

    [YSA-10] Feature: YakssokButton 구현

---
+++ core/designsystem/src/main/kotlin/com/pillsquad/yakssok/core/designsystem/component/YakssokButton.kt
@@ -0,0 +1,72 @@
+package com.pillsquad.yakssok.core.designsystem.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.pillsquad.yakssok.core.designsystem.theme.YakssokTheme
+
+@Composable
+fun YakssokButton(
+    modifier: Modifier = Modifier,
+    @DrawableRes icon: Int? = null,
+    text: String,
+    enabled: Boolean = true,
+    round: Dp = 16.dp,
+    textStyle: TextStyle = YakssokTheme.typography.subtitle2,
+    backgroundColor: Color = YakssokTheme.color.primary400,
+    contentColor: Color = YakssokTheme.color.grey500,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor,
+            contentColor = contentColor
+        ),
+        contentPadding = PaddingValues(16.dp),
+        shape = RoundedCornerShape(round),
+        enabled = enabled,
+        onClick = onClick
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            icon?.let {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Icon(
+                        painter = painterResource(id = icon),
+                        contentDescription = "login button"
+                    )
+                }
+            }
+
+            Text(
+                text = text,
+                style = textStyle
+            )
+        }
+    }
+}
