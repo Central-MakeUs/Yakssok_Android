20c8bcf267b09eeaddd0d23d1a117e4f54757435
Inwoo <lys91892@gmail.com>
2025-07-17 02:12:42 +0900

    [YSA-31] Feature: 왼쪽 꼬리 말풍선 구현

--- feature/alert/src/main/java/com/pillsquad/yakssok/feature/alert/AlertScreen.kt
+++ feature/alert/src/main/java/com/pillsquad/yakssok/feature/alert/AlertScreen.kt
@@ -1,14 +1,17 @@
 package com.pillsquad.yakssok.feature.alert
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pillsquad.yakssok.core.designsystem.component.YakssokTopAppBar
 import com.pillsquad.yakssok.core.designsystem.theme.YakssokTheme
 import com.pillsquad.yakssok.core.ui.ext.yakssokDefault
+import com.pillsquad.yakssok.feature.alert.component.MessageBubble
 
 @Composable
 internal fun AlertRoute(
@@ -32,10 +35,16 @@ private fun AlertScreen(
             title = "알림",
             onBackClick = onNavigateBack
         )
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth().weight(1f)
+        Box(
+            modifier = Modifier.weight(1f).fillMaxWidth(),
+            contentAlignment = Alignment.Center
         ) {
-
+            MessageBubble("일이삼사오육칠팔구십일이삼사오")
         }
+//        LazyColumn(
+//            modifier = Modifier.fillMaxWidth().weight(1f)
+//        ) {
+//
+//        }
     }
 }
