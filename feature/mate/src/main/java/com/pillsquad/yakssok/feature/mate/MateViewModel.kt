871b38a6c85f711a5a7580204a89f57a4e21c9f4
Inwoo <lys91892@gmail.com>
2025-07-17 17:32:50 +0900

    [YSA-29] Feature: 메이트 UI 구현

--- feature/mate/src/main/java/com/pillsquad/yakssok/feature/mate/MateViewModel.kt
+++ feature/mate/src/main/java/com/pillsquad/yakssok/feature/mate/MateViewModel.kt
@@ -1,11 +1,23 @@
 package com.pillsquad.yakssok.feature.mate
 
 import androidx.lifecycle.ViewModel
+import com.pillsquad.yakssok.feature.mate.model.MateUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class MateViewModel @Inject constructor(
 
 ): ViewModel() {
+    private var _uiState = MutableStateFlow(MateUiModel())
+    val uiState = _uiState.asStateFlow()
+
+    fun updateInputCode(newCode: String) {
+        _uiState.update {
+            it.copy(inputCode = newCode)
+        }
+    }
 }
---
+++ feature/mate/src/main/java/com/pillsquad/yakssok/feature/mate/model/MateUiModel.kt
@@ -0,0 +1,6 @@
+package com.pillsquad.yakssok.feature.mate.model
+
+data class MateUiModel(
+    val myCode: String = "abcdef1gh",
+    val inputCode: String = ""
+)
