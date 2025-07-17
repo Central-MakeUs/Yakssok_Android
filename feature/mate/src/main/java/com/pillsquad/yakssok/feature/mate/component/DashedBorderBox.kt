507b256654e61510dc3796b80dd589f10ffdfa45
Inwoo <lys91892@gmail.com>
2025-07-17 17:32:38 +0900

    [YSA-29] Feature: 점선형 박스 구현

---
+++ feature/mate/src/main/java/com/pillsquad/yakssok/feature/mate/component/DashedBorderBox.kt
@@ -0,0 +1,50 @@
+package com.pillsquad.yakssok.feature.mate.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.pillsquad.yakssok.core.designsystem.theme.YakssokTheme
+
+@Composable
+internal fun DashedBorderBox(
+    modifier: Modifier = Modifier,
+    cornerRadius: Dp = 16.dp,
+    strokeWidth: Dp = 1.dp,
+    dashLength: Float = 7f,
+    gapLength: Float = 8f,
+    borderColor: Color = YakssokTheme.color.grey400,
+    content: @Composable BoxScope.() -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 32.dp)
+            .drawBehind {
+                val stroke = Stroke(
+                    width = strokeWidth.toPx(),
+                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(dashLength, gapLength), 0f)
+                )
+                drawRoundRect(
+                    color = borderColor,
+                    size = size,
+                    style = stroke,
+                    cornerRadius = CornerRadius(cornerRadius.toPx())
+                )
+            }
+            .padding(vertical = (14.5).dp),
+        contentAlignment = Alignment.Center
+    ) {
+        content()
+    }
+}
