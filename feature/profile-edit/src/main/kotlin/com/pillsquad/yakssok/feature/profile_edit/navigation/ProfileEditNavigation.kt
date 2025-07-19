5ab293d5bcfb2a9def25510baa4f783190749417
Inwoo <lys91892@gmail.com>
2025-07-19 17:34:34 +0900

    [YSA-37] Feature: Profile edit ui 구현

--- feature/profile-edit/src/main/kotlin/com/pillsquad/yakssok/feature/profile_edit/navigation/ProfileEditNavigation.kt
+++ feature/profile-edit/src/main/kotlin/com/pillsquad/yakssok/feature/profile_edit/navigation/ProfileEditNavigation.kt
@@ -1,10 +1,11 @@
-package com.pillsquad.yakssok.feature.profile_edit
+package com.pillsquad.yakssok.feature.profile_edit.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.pillsquad.yakssok.core.navigation.Route
+import com.pillsquad.yakssok.feature.profile_edit.ProfileEditRoute
 
 fun NavController.navigateProfileEdit(navOptions: NavOptions) {
     navigate(route = Route.ProfileEdit, navOptions = navOptions)
@@ -14,6 +15,8 @@ fun NavGraphBuilder.profileEditNavGraph(
     onNavigateBack: () -> Unit
 ) {
     composable<Route.ProfileEdit> {
-
+        ProfileEditRoute(
+            onNavigateBack = onNavigateBack
+        )
     }
 }
