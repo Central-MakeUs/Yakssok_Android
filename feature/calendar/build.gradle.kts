95e0974ab78a2343c002ae91638c86c1838fdba1
Inwoo <lys91892@gmail.com>
2025-07-23 00:48:59 +0900

    [YSA-42] Feature: Calenar 모듈 생성

---
+++ feature/calendar/build.gradle.kts
@@ -0,0 +1,11 @@
+import com.pillsquad.yakssok.setNamespace
+
+plugins {
+    alias(libs.plugins.yakssok.android.feature)
+}
+
+setNamespace("feature.calendar")
+
+dependencies {
+    implementation(libs.kotlinx.datetime)
+}
