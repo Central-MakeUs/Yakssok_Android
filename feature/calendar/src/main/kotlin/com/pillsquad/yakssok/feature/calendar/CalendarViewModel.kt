e2651fbed54bacbf2bbe77249205b2b76dd55105
Inwoo <lys91892@gmail.com>
2025-07-23 01:42:56 +0900

    [YSA-43] Feature: calendar 제외 UI 구현

--- feature/calendar/src/main/kotlin/com/pillsquad/yakssok/feature/calendar/CalendarViewModel.kt
+++ feature/calendar/src/main/kotlin/com/pillsquad/yakssok/feature/calendar/CalendarViewModel.kt
@@ -3,9 +3,13 @@ package com.pillsquad.yakssok.feature.calendar
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class CalendarViewModel @Inject constructor(
 ): ViewModel(){
+    private val _uiState = MutableStateFlow(CalendarUiModel())
+    val uiState = _uiState.asStateFlow()
+
 }
