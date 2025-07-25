f1daf512c1746d571e227a85529943d4f20fd69b
Inwoo <lys91892@gmail.com>
2025-07-25 14:05:59 +0900

    [YSA-44] Feature: 로그인/회원가입 흐름 정리

--- feature/intro/src/main/java/com/pillsquad/yakssok/feature/intro/IntroViewModel.kt
+++ feature/intro/src/main/java/com/pillsquad/yakssok/feature/intro/IntroViewModel.kt
@@ -1,7 +1,12 @@
 package com.pillsquad.yakssok.feature.intro
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import com.pillsquad.yakssok.feature.intro.model.IntroUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
@@ -23,7 +28,7 @@ class IntroViewModel @Inject constructor(
     }
 
     fun deleteLoginInfo() {
-        _uiState.update { it.copy(isLogin = false, nickName = "", isEnabled = false) }
+        _uiState.update { it.copy(isHaveToSignup = false, nickName = "", isEnabled = false) }
     }
 
     fun changeNickName(nickName: String) {
@@ -36,8 +41,41 @@ class IntroViewModel @Inject constructor(
         _uiState.update { it.copy(isLoading = true) }
     }
 
-    fun handleSignIn() {
-        _uiState.update { it.copy(isLogin = true) }
+    fun handleSignIn(context: Context) {
+        val kakaoInstance = UserApiClient.instance
+
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null || token == null) {
+                // Snackbar or Toast 띄우기
+            } else {
+                // server에 accessToken 전달
+            }
+        }
+
+        if (kakaoInstance.isKakaoTalkLoginAvailable(context)) {
+            kakaoInstance.loginWithKakaoTalk(context) { token, error ->
+                // 로그인 실패 처리
+                if (error != null || token == null) {
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        // Snackbar 띄우기 or Toast 띄우기
+                        return@loginWithKakaoTalk
+                    }
+                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                } else {
+                    // 로그인 성공 처리
+                    // server에 accessToken 전달
+                }
+            }
+        } else {
+            kakaoInstance.loginWithKakaoAccount(context, callback = callback)
+        }
+    }
+
+    private fun loginUser(accessToken: String) {
+        viewModelScope.launch {
+            // server에 accessToken 전달
+            _uiState.update { it.copy(isLoading = false) }
+        }
     }
 
     private fun validateNickName(nickName: String): Boolean {
@@ -47,6 +85,7 @@ class IntroViewModel @Inject constructor(
 
     private fun checkToken() {
         viewModelScope.launch {
+            // datastore에서 token 존재하는지 확인
             delay(1500)
             _uiState.update { it.copy(isLoading = false) }
         }
--- feature/intro/src/main/java/com/pillsquad/yakssok/feature/intro/model/IntroUiModel.kt
+++ feature/intro/src/main/java/com/pillsquad/yakssok/feature/intro/model/IntroUiModel.kt
@@ -2,7 +2,9 @@ package com.pillsquad.yakssok.feature.intro.model
 
 data class IntroUiModel(
     val isLoading: Boolean = true,
-    val isLogin: Boolean = false,
+    val isHaveToSignup: Boolean = false,
     val isEnabled: Boolean = false,
-    val nickName: String = ""
+    val nickName: String = "",
+    val token: String = "",
+    val loginSuccess: Boolean = false
 )
