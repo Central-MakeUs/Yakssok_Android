b6f06880086d46db4a01ddf8d9accf794e0f2721
Inwoo <lys91892@gmail.com>
2025-07-25 23:26:28 +0900

    [YSA-44] Feature: ResponseParser.kt 수정

--- core/network/src/main/kotlin/com/pillsquad/yakssok/core/network/util/ResponseParser.kt
+++ core/network/src/main/kotlin/com/pillsquad/yakssok/core/network/util/ResponseParser.kt
@@ -1,8 +1,11 @@
 package com.pillsquad.yakssok.core.network.util
 
+import android.util.Log
 import com.pillsquad.yakssok.core.network.model.ApiResponse
 import com.pillsquad.yakssok.core.network.model.BaseResponse
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import okio.IOException
@@ -28,22 +31,27 @@ internal fun <T> Response<BaseResponse<T>>.asApiResponse(): ApiResponse<T> {
 private fun <T> parseFailureResponse(
     response: Response<BaseResponse<T>>,
 ): ApiResponse.Failure {
-
     val errorStr = response.errorBody()?.string().orEmpty()
 
-    val message = try {
-        Json.parseToJsonElement(errorStr).jsonObject["message"]?.jsonPrimitive?.content
-            ?: "Unknown error"
+    return try {
+        val errorResponse = Json.decodeFromString(BaseResponse.serializer(JsonElement.serializer()), errorStr)
+
+        val code = errorResponse.code
+        val message = errorResponse.message
+
+        ApiResponse.Failure.HttpError(
+            code = code,
+            message = message,
+            body = errorResponse.body?.toString() ?: "null"
+        )
     } catch (e: Exception) {
         e.printStackTrace()
-        "파싱 실패: ${e.localizedMessage}"
+        ApiResponse.Failure.HttpError(
+            code = response.code().toLong(),
+            message = "파싱 실패: ${e.localizedMessage}",
+            body = errorStr
+        )
     }
-
-    return ApiResponse.Failure.HttpError(
-        code = response.code().toLong(),
-        message = message,
-        body = errorStr
-    )
 }
 
 @Suppress("UNCHECKED_CAST")
@@ -55,8 +63,6 @@ private fun <T> parseSuccessResponse(
     if (base == null) {
         println("base is null despite successful response")
     }
-
-    println("base: $base")
 
     if (base?.code == 0L) {
         return if (base.body != null) {
