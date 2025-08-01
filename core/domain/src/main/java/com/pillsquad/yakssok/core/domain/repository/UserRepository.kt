8e58c2d130f3add3dfc9c870992a659e132b1d4c
Inwoo <lys91892@gmail.com>
2025-08-01 20:33:25 +0900

    [YSA-52] Feature: repository, usecase 구현
    - 메이트 등록 관련 중 Get 관련

--- core/domain/src/main/java/com/pillsquad/yakssok/core/domain/repository/UserRepository.kt
+++ core/domain/src/main/java/com/pillsquad/yakssok/core/domain/repository/UserRepository.kt
@@ -1,8 +1,11 @@
 package com.pillsquad.yakssok.core.domain.repository
 
 import com.pillsquad.yakssok.core.model.User
+import com.pillsquad.yakssok.core.model.UserInfo
 
 interface UserRepository {
     suspend fun postMyInfoToLocal()
     suspend fun getMyInfo(): User
+    suspend fun getMyInviteCode(): String
+    suspend fun getUserInfoByInviteCode(inviteCode: String): Result<UserInfo>
 }
