b10b07eb3b254e416238780bae912cb2b8846dd1
Inwoo <lys91892@gmail.com>
2025-08-01 21:44:23 +0900

    [YSA-52] Feature: 클립보드 복사 구현

--- feature/mate/src/main/java/com/pillsquad/yakssok/feature/mate/MateScreen.kt
+++ feature/mate/src/main/java/com/pillsquad/yakssok/feature/mate/MateScreen.kt
@@ -1,6 +1,9 @@
 package com.pillsquad.yakssok.feature.mate
 
+import android.content.ClipData
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -18,11 +21,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.Clipboard
+import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.platform.toClipEntry
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
@@ -35,6 +44,8 @@ import com.pillsquad.yakssok.core.designsystem.theme.YakssokTheme
 import com.pillsquad.yakssok.feature.mate.component.DashedBorderBox
 import com.pillsquad.yakssok.feature.mate.component.MateCompleteDialog
 import com.pillsquad.yakssok.feature.mate.model.MateUiModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun MateRoute(
@@ -128,7 +139,7 @@ private fun MateScreen(
 
         CodeContent(
             modifier = Modifier.weight(1f),
-            code = uiState.myCode
+            code = uiState.myCode,
         )
     }
 }
@@ -167,8 +178,11 @@ private fun IconBox(
 @Composable
 private fun CodeContent(
     modifier: Modifier,
-    code: String
+    code: String,
 ) {
+    val clipboard = LocalClipboard.current
+    val scope = rememberCoroutineScope()
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -198,12 +212,26 @@ private fun CodeContent(
         Spacer(modifier = Modifier.height(9.dp))
 
         DashedBorderBox {
-            Text(
-                text = code,
-                textDecoration = TextDecoration.Underline,
-                style = YakssokTheme.typography.body0,
-                color = YakssokTheme.color.grey950
-            )
+            Box(
+                modifier = Modifier.clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = {
+                        val clipData = ClipData.newPlainText("초대코드", code)
+
+                        scope.launch {
+                            clipboard.setClipEntry(clipData.toClipEntry())
+                        }
+                    }
+                )
+            ) {
+                Text(
+                    text = code,
+                    textDecoration = TextDecoration.Underline,
+                    style = YakssokTheme.typography.body0,
+                    color = YakssokTheme.color.grey950
+                )
+            }
         }
 
         Spacer(modifier = Modifier.weight(1f))
