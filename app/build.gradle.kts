06151b51451d68641039f8d4e8e3d88d49c2ff13
Inwoo <lys91892@gmail.com>
2025-08-03 23:31:58 +0900

    [YSA-9] Feature: app version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 
     defaultConfig {
         applicationId = "com.pillsquad.yakssok"
-        versionCode = 6
-        versionName = "0.0.6"
+        versionCode = 7
+        versionName = "0.0.7"
         targetSdk = 36
 
         buildConfigField("String", "KAKAO_API_KEY", getProperty("KAKAO_API_KEY"))
