7ff361b6a695dec6e25b51747d666d3c47a6d2f9
Inwoo <lys91892@gmail.com>
2025-08-08 02:52:34 +0900

    [YSA-57] Feature: 메이트, 복약 추가 로컬 적용

--- core/data/src/main/kotlin/com/pillsquad/yakssok/core/data/repository/MedicationRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/pillsquad/yakssok/core/data/repository/MedicationRepositoryImpl.kt
@@ -7,15 +7,27 @@ import com.pillsquad.yakssok.core.domain.repository.MedicationRepository
 import com.pillsquad.yakssok.core.model.Medication
 import com.pillsquad.yakssok.core.model.MyRoutine
 import com.pillsquad.yakssok.core.network.datasource.MedicationDataSource
+import com.pillsquad.yakssok.datastore.UserLocalDataSource
+import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 
 class MedicationRepositoryImpl @Inject constructor(
-    private val medicationDataSource: MedicationDataSource
+    private val medicationDataSource: MedicationDataSource,
+    private val userLocalDataSource: UserLocalDataSource
 ) : MedicationRepository {
     override suspend fun postMedication(medication: Medication): Result<Unit> {
         val params = medication.toPostMedicationRequest()
 
-        return medicationDataSource.postMedication(params = params).toResult()
+        val result = medicationDataSource.postMedication(params = params).toResult(
+            transform = { it }
+        )
+
+        result.onSuccess {
+            val currentCount = userLocalDataSource.medicationCountFlow.firstOrNull() ?: 0
+            userLocalDataSource.saveMedicationCount(currentCount + 1)
+        }
+
+        return result
     }
 
     override suspend fun putEndMedication(medicationId: Long): Result<Unit> {
