4a7b3ac8101b9bbedf3018922a0378a46895c60d
Inwoo <lys91892@gmail.com>
2025-08-15 03:18:59 +0900

    [YSA-64] Feature: 약 타입 순서 변경 및 글씨체 명확히 나눔

--- core/model/src/main/kotlin/com/pillsquad/yakssok/core/model/MedicationType.kt
+++ core/model/src/main/kotlin/com/pillsquad/yakssok/core/model/MedicationType.kt
@@ -1,11 +1,11 @@
 package com.pillsquad.yakssok.core.model
 
 enum class MedicationType(val krName: String, val color: Long) {
-    MENTAL("정신 건강 관리", 0xFF7C24DB),
-    BEAUTY("미용 관련 관리", 0xFF3ADE4D),
+    SUPPLEMENT("건강 기능 식품/영양보충", 0xFFDB7C24),
     CHRONIC("만성 질환 관리", 0xFF40B0FA),
+    BEAUTY("미용 관련 관리", 0xFF3ADE4D),
     DIET("다이어트/대사 관련", 0xFFD224DB),
     TEMPORARY("통증/감기 등 일시적 치료", 0xFFE89F18),
-    SUPPLEMENT("건강 기능 식품/영양보충", 0xFFDB7C24),
+    MENTAL("정신 건강 관리", 0xFF7C24DB),
     OTHER("기타 설정", 0xFFDB2427),
 }
