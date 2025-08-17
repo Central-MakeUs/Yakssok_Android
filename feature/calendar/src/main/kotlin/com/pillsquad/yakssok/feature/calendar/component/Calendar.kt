f9fe5d481523ef9893115c4ff4188a809566cd1a
Inwoo <lys91892@gmail.com>
2025-08-17 18:03:53 +0900

    [YSA-65] Feature: 캘린더 요일 수정

--- feature/calendar/src/main/kotlin/com/pillsquad/yakssok/feature/calendar/component/Calendar.kt
+++ feature/calendar/src/main/kotlin/com/pillsquad/yakssok/feature/calendar/component/Calendar.kt
@@ -1,6 +1,5 @@
 package com.pillsquad.yakssok.feature.calendar.component
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
