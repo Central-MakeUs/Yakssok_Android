d87fd04eaedadc6089f6fa4dbdb53d5c0387e54e
Inwoo <lys91892@gmail.com>
2025-08-20 21:51:54 +0900

    [YSA-63] Feature: widget 모듈 생성

---
+++ widget/build.gradle.kts
@@ -0,0 +1,16 @@
+import com.pillsquad.yakssok.setNamespace
+
+plugins {
+    alias(libs.plugins.yakssok.android.library)
+    alias(libs.plugins.yakssok.android.compose)
+}
+
+setNamespace("widget")
+
+dependencies {
+    implementation(projects.core.domain)
+    implementation(projects.core.model)
+
+    implementation(libs.androidx.glance.appwidget)
+    implementation(libs.androidx.glance.material3)
+}
