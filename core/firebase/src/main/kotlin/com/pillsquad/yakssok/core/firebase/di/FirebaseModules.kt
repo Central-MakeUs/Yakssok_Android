8ec706dcc8f67694f2a1c10589e6fdd86d8cf1e9
Inwoo <lys91892@gmail.com>
2025-10-04 14:42:10 +0900

    [YSA-71] Feature: RemoteCOnfigProvider 구현

---
+++ core/firebase/src/main/kotlin/com/pillsquad/yakssok/core/firebase/di/FirebaseModules.kt
@@ -0,0 +1,52 @@
+package com.pillsquad.yakssok.core.firebase.di
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
+import com.pillsquad.yakssok.core.firebase.DeviceIdProvider
+import com.pillsquad.yakssok.core.firebase.DeviceIdProviderImpl
+import com.pillsquad.yakssok.core.firebase.FcmTokenProvider
+import com.pillsquad.yakssok.core.firebase.FcmTokenProviderImpl
+import com.pillsquad.yakssok.core.firebase.RemoteConfigProvider
+import com.pillsquad.yakssok.core.firebase.RemoteConfigProviderImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class FirebaseModules {
+    @Binds
+    @Singleton
+    abstract fun bindDeviceIdProvider(impl: DeviceIdProviderImpl): DeviceIdProvider
+
+    @Binds
+    @Singleton
+    abstract fun bindFcmTokenProvider(impl: FcmTokenProviderImpl): FcmTokenProvider
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteConfigProvider(impl: RemoteConfigProviderImpl): RemoteConfigProvider
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideFirebaseRemoteConfig(): FirebaseRemoteConfig {
+            val remoteConfig = FirebaseRemoteConfig.getInstance()
+            val configSettings = remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 3600
+            }
+            remoteConfig.setConfigSettingsAsync(configSettings)
+
+            val defaults = mapOf(
+                "min_supported_version" to "1.0.0",
+                "recommended_version" to "1.0.0"
+            )
+            remoteConfig.setDefaultsAsync(defaults)
+
+            return remoteConfig
+        }
+    }
+}
